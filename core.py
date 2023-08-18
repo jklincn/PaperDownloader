c573a04b676e6a0878d2501f39f4513995ca2a10
jklincn <jklincn@outlook.com>
2023-08-18 22:44:05 +0800

    add GUI and pack to EXE

---
+++ core.py
@@ -0,0 +1,202 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common import exceptions
+import os
+import shutil
+import time
+import subprocess
+
+# Parameters
+WaitTime = 5
+
+# Global Variables
+check_count = 0
+download_count = 0
+
+
+class Browse:
+    def __init__(self):
+        self.name = ""
+        self.exe_path = ""
+
+    def open(self):
+        pass
+
+
+class Chrome(Browse):
+    def __init__(self, ChromePath=""):
+        self.name = "Google Chrome"
+        self.exe_path = ChromePath
+        self.user_data_path = os.environ["TEMP"] + "/chrome_temp_data"
+        self.port = "16888"
+
+    def open(self):
+        if not os.path.exists(self.exe_path):
+            print("错误: 找不到 chrome.exe, 请手动设置 ChromePath。\n")
+            return
+        # Create temporary folder
+        if not os.path.exists(self.user_data_path):
+            os.makedirs(self.user_data_path)
+        subprocess.run(
+            [
+                self.exe_path,
+                "--remote-debugging-port={}".format(self.port),
+                "--user-data-dir={}".format(self.user_data_path),
+            ]
+        )
+
+    def driver(self) -> webdriver:
+        # Set browser options
+        ChromeOptions = webdriver.ChromeOptions()
+        ChromeOptions.add_experimental_option(
+            "debuggerAddress", "127.0.0.1:{}".format(self.port)
+        )
+        service = webdriver.ChromeService(executable_path="chromedriver.exe")
+        driver = webdriver.Chrome(options=ChromeOptions,service=service)
+        return driver
+
+    def clean(self):
+        if os.path.exists(self.user_data_path):
+            shutil.rmtree(self.user_data_path)
+
+
+def download(driver: webdriver,Interval):
+    # Find serach window
+    all_handles = driver.window_handles
+    hit = False
+    for handle in all_handles:
+        driver.switch_to.window(handle)
+        if driver.title == "检索-中国知网":
+            cnki(driver,Interval)
+            hit = True
+        elif driver.title == "高级检索-中国知网":
+            cnki(driver,Interval)
+            hit = True
+        elif driver.title == "万方数据知识服务平台":
+            # Ensure we are now in search window
+            try:
+                driver.find_element(By.CLASS_NAME, "normal-list")
+            except exceptions.NoSuchElementException:
+                continue
+            else:
+                wanfang(driver)
+                hit = True
+    if hit:
+        print("下载完成, 共找到 {} 处勾选, 成功下载 {} 项内容。\n".format(download_count, check_count))
+    else:
+        print("错误: 没有找到符合的检索页面\n")
+
+
+def cnki(driver: webdriver,Interval):
+    print("检测到知网检索页面, 开始下载......\n")
+    global download_count, check_count
+    index_window = driver.current_window_handle
+    result = driver.find_element(By.CLASS_NAME, "result-table-list").find_element(
+        By.TAG_NAME, "tbody"
+    )
+    rows = result.find_elements(By.TAG_NAME, "tr")
+    for i in range(len(rows)):
+        # Determine if selected
+        if not rows[i].find_element(By.CLASS_NAME, "cbItem").is_selected():
+            continue
+        else:
+            check_count += 1
+            current_window_number = len(driver.window_handles)
+            rows[i].find_element(By.CLASS_NAME, "fz14").click()
+            WebDriverWait(driver, WaitTime).until(
+                EC.number_of_windows_to_be(current_window_number + 1)
+            )
+            # Switch to new window
+            driver.switch_to.window(driver.window_handles[-1])
+            WebDriverWait(driver, WaitTime).until(
+                EC.element_to_be_clickable((By.ID, "pdfDown"))
+            )
+            try:
+                download_button = driver.find_element(By.ID, "pdfDown")
+            except exceptions.NoSuchElementException:
+                name = rows[i].find_element(By.CLASS, "wx-tit").text
+                print("错误: 不能下载 {}\n。".format(name))
+                continue
+            else:
+                current_window_number = len(driver.window_handles)
+                download_button.click()
+                try:
+                    WebDriverWait(driver, WaitTime).until(
+                        EC.number_of_windows_to_be(current_window_number)
+                    )
+                except exceptions.TimeoutException:
+                    print("错误: 账号未登录，下载中断。\n")
+                    return
+                else:
+                    driver.close()
+                    # Switch back to index
+                    driver.switch_to.window(index_window)
+                    download_count += 1
+                    time.sleep(Interval)
+
+
+def wanfang(driver: webdriver,Interval):
+    print("检测到万方检索页面, 开始下载......\n")
+    global download_count, check_count
+    index_window = driver.current_window_handle
+    rows = driver.find_elements(By.CLASS_NAME, "normal-list")
+    for i in range(len(rows)):
+        # Determine if selected
+        try:
+            rows[i].find_element(By.CLASS_NAME, "checkbox.active")
+        except exceptions.NoSuchElementException:
+            continue
+        else:
+            check_count += 1
+            current_window_number = len(driver.window_handles)
+            try:
+                download_button = rows[i].find_element(
+                    By.CSS_SELECTOR,
+                    "div:nth-child({}) > .normal-list .t-DIB:nth-child(2) span".format(
+                        str(i + 1)
+                    ),
+                )
+            except exceptions.NoSuchElementException:
+                name = rows[i].find_element(By.CLASS_NAME, "title").text
+                print("错误: 不能下载 {}。\n".format(name))
+                continue
+            else:
+                download_button.click()
+                # Switch to new window
+                WebDriverWait(driver, WaitTime).until(
+                    EC.number_of_windows_to_be(current_window_number + 1)
+                )
+                driver.switch_to.window(driver.window_handles[-1])
+                if driver.title == "万方登录":
+                    print("错误: 账号未登录，下载中断。\n")
+                    return
+                else:
+                    time.sleep(Interval)
+                    driver.close()
+                    # Switch back to index
+                    driver.switch_to.window(index_window)
+                    download_count += 1
+
+
+# def usage():
+#     print("usage: python run.py <command>")
+#     print("Optional commands are as follows:")
+#     print("  prepare    Open the browser and prepare the website")
+#     print("  download   Start automated download")
+#     print("  clean      Delete temporary data directory")
+
+
+# if __name__ == "__main__":
+#     if len(sys.argv) == 2:
+#         if sys.argv[1] == "prepare":
+#             prepare()
+#         elif sys.argv[1] == "download":
+#             download()
+#         elif sys.argv[1] == "clean":
+#             clean()
+#         else:
+#             usage()
+#     else:
+#         usage()
---
+++ gui.py
@@ -0,0 +1,176 @@
+from functools import partial
+import core
+import sys
+import ctypes
+import time
+from tkinter import *
+from tkinter import ttk
+from tkinter import scrolledtext
+from threading import Thread
+
+VERSION = "v1.0.0"
+
+
+class StdoutRedirector(object):
+    def __init__(self, text_widget):
+        self.text_space = text_widget
+
+    def write(self, str):
+        self.text_space.insert("end", str)
+        self.text_space.update()
+
+    def flush(self):
+        pass
+
+
+def window_set_to_center(win):
+    sw = win.winfo_screenwidth()
+    sh = win.winfo_screenheight()
+    w = 800
+    h = 600
+    win.geometry("%dx%d+%d+%d" % (w, h, (sw - w) / 2, (sh - h) / 2))
+    # win.resizable(0, 0)
+
+
+def high_resolution(win):
+    ctypes.windll.shcore.SetProcessDpiAwareness(1)
+    # 获取屏幕的缩放因子
+    ScaleFactor = ctypes.windll.shcore.GetScaleFactorForDevice(0)
+    # 设置程序缩放
+    win.tk.call("tk", "scaling", ScaleFactor / 75)
+
+
+def check_browse_alive(t_browse: Thread):
+    while 1:
+        if not t_browse.is_alive():
+            print("检测到浏览器已关闭。\n")
+            b_download["state"] = DISABLED
+            b_clean["state"] = NORMAL
+            b_setting["state"] = NORMAL
+            return
+        time.sleep(0.1)
+
+
+def bf_browse(browse_name):
+    global browse
+    if browse_name == "chrome":
+        print("使用浏览器: Google Chrome。\n")
+        browse = core.Chrome(ChromePath)
+    else:
+        print("错误：不支持的浏览器\n")
+    # todo: 将异常从子线程转移到主线程处理
+    t_browse = Thread(target=browse.open, daemon=True)
+    t_browse.start()
+    t_browse_alive = Thread(target=check_browse_alive, args=[t_browse], daemon=True)
+    t_browse_alive.start()
+    b_download["state"] = NORMAL
+    b_clean["state"] = DISABLED
+    b_setting["state"] = DISABLED
+    print("请登录数据库网站进行内容检索, 在需要下载的论文前面打上勾, 点击【下载】按钮。\n")
+
+
+def bf_download():
+    print("正在接管浏览器控制，请不要操作。\n", flush=True)
+    core.download(browse.driver(),int(Interval))
+    print("浏览器接管结束, 可以继续操作。\n", flush=True)
+
+
+def bf_exit():
+    win.destroy()
+
+
+def log_bar(win):
+    output = scrolledtext.ScrolledText(win, font=("微软雅黑", 12), width=1)
+    output.pack(expand=True, fill=BOTH, side=LEFT)
+    sys.stdout = StdoutRedirector(output)
+    print("=======================================")
+    print("欢迎使用论文批量下载器！")
+    print("当前版本: {}".format(VERSION))
+    print("=======================================")
+    print("由于作者水平原因(为了自适应高分辨率), 请自行缩放窗口大小并拖动到合适位置")
+    print("=======================================\n")
+    print("请在右侧选择使用的浏览器。\n")
+
+
+def bf_setting():
+    win_setting = Toplevel()
+    win_setting.title("设置")
+    window_set_to_center(win_setting)
+
+    value = {
+        "chrome_path": StringVar(value=ChromePath),
+        "interval": StringVar(value="3"),
+    }
+
+    # Google Chrome 可执行文件路径
+    f_chrome_path = ttk.Frame(win_setting)
+    f_chrome_path.pack(padx=20, expand=True)
+    ttk.Label(f_chrome_path, text="Google Chrome 可执行文件路径: ", font=("微软雅黑", 12)).pack(
+        side=LEFT
+    )
+    Entry(f_chrome_path, textvariable=value["chrome_path"], font=('微软雅黑 12'), width=60).pack(side=LEFT)
+
+    # 下载间隔时间
+    f_interval = ttk.Frame(win_setting)
+    f_interval.pack(padx=20, expand=True)
+    ttk.Label(f_interval, text="下载间隔时间(秒): ", font=("微软雅黑", 12)).pack(side=LEFT)
+    Entry(f_interval, textvariable=value["interval"], width=10, font=('微软雅黑 12')).pack(side=LEFT)
+
+    # 确定按钮
+    ttk.Button(
+        win_setting, text="确定", command=partial(bf_setting_done, win_setting, value)
+    ).pack(expand=True)
+
+
+def bf_setting_done(win_setting, value):
+    global ChromePath
+    ChromePath = value["chrome_path"].get()
+    Interval = value["interval"].get()
+    win_setting.destroy()
+
+
+def bf_clean():
+    core.Chrome().clean()
+    print("浏览器所有设置已重置, 用户数据已清理。\n")
+
+
+if __name__ == "__main__":
+    win = Tk()
+    win.title("论文批量下载器")
+
+    browse = None
+    ChromePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+    Interval = "3"
+
+    # todo：适配高分辨率
+    high_resolution(win)
+
+    # 设置窗口大小与位置
+    window_set_to_center(win)
+
+    log_bar(win)
+
+    browse_frame = ttk.Frame(win)
+    browse_frame.pack(padx=20, pady=20, expand=True)
+    ttk.Label(browse_frame, text="可选浏览器", font=("微软雅黑", 15)).pack()
+
+    # 按钮组
+    # Google Chrome
+    b_google_chrome = ttk.Button(
+        browse_frame, text="Google Chrome", command=partial(bf_browse, "chrome")
+    )
+    b_google_chrome.pack(padx=20, pady=20, expand=True, ipadx=20)
+    # 下载
+    b_download = ttk.Button(win, text="下载", state=DISABLED, command=bf_download)
+    b_download.pack(padx=20, pady=20, expand=True)
+    # 设置
+    b_setting = ttk.Button(win, text="设置", command=bf_setting)
+    b_setting.pack(padx=20, pady=20, expand=True)
+    # 浏览器重置
+    b_clean = ttk.Button(win, text="浏览器重置", command=bf_clean)
+    b_clean.pack(padx=20, pady=20, expand=True)
+    # 退出
+    b_exit = ttk.Button(win, text="退出", command=bf_exit)
+    b_exit.pack(padx=20, pady=20, expand=True)
+
+    win.mainloop()
